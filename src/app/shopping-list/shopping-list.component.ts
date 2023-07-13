9073389cd3a834e19feb5e2a19029da1da300351
JerryUhm <acdc204@gmail.com>
2023-07-13 15:58:11 +0900

    learn update

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [];
 
+  constructor() { 
+    this.ingredients.push(new Ingredient('Apples', 5));
+    this.ingredients.push(new Ingredient('Tomatoes', 10));
+    this.ingredients.push(new Ingredient('Banana', 5));
+
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
